0a06bd9669fb587df1dce28895d2fb4a30cc561a
Miquel Gomis <miquel.gomis@fdsa.es>
2022-02-24 18:18:23 +0100

    Fix

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@ import MapDrawing from './components/MapDrawing';
 function App() {
   return (
     <div>
-      <h1>Map</h1>
       <Map />
 
-      <br />
-      <h1>Map Drawing</h1>
-      <MapDrawing />
+      {
+        /*
+        <br />
+        <h1>Map Drawing</h1>
+        <MapDrawing />
+        */
+      }
     </div>
   );
 }
